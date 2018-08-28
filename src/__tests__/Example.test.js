cdbe0fe12aed58d415fa32a5fb79b9c0a26e6a1c
asamisellah <asami.rachael@gmail.com>
2018-08-28 15:32:50 +0300

    [ch #159961944] Create tests folder and Example test

---
+++ src/__tests__/Example.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hello from "../exampleComponent";
+import { shallow } from "enzyme";
+
+it("renders the name given", () => {
+  const wrapper = shallow(<Hello name={"Jack"} />);
+  expect(wrapper.find("p").text()).toEqual("Hello, Jack!");
+});
+
+it("returns 'Unknown' if no name is passed in", () => {
+  const wrapper = shallow(<Hello />);
+  expect(wrapper.find("p").text()).toEqual("Hello, Unknown!");
+});
