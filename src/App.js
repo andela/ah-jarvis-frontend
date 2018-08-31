d22039f413d76e4986609b56c3626c750721be7b
Duncanian <ianduncan08@gmail.com>
2018-08-31 13:34:10 +0300

    #159961942 Setup Airbnb Eslint (#4)
    * [#159961942] Add Airbnb eslint
    
    * [#159961942] Replace component with an arrow function
    
    * [#159961942] Resolve airbnb-eslint dependencies
    
    * [ch #159961942] Remove package-lock
    
    * [#159961942] Add linting command
    
    * [ch #159961942] Allow use of double quotes alone
    
    * [ch #159961942] Correct linting errors and warnings
    
    * [ch #159961942] Resolve failing tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Authors' Haven</h1>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome to Authors' Haven</h1>
+    </header>
+  </div>
+);
 
 export default App;
--- src/testExample/Example.test.js
+++ src/testExample/Example.test.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Hello from "./exampleComponent";
-import { shallow } from "enzyme";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Hello from './exampleComponent';
 
-it("renders the name given", () => {
-  const wrapper = shallow(<Hello name={"Jack"} />);
-  expect(wrapper.find("p").text()).toEqual("Hello, Jack!");
+it('renders the name given', () => {
+  const wrapper = shallow(<Hello name="Jack" />);
+  expect(wrapper.find('p').text()).toEqual('Hello, Jack!');
 });
 
-it("returns 'Unknown' if no name is passed in", () => {
+it('returns \'Unknown\' if no name is passed in', () => {
   const wrapper = shallow(<Hello />);
-  expect(wrapper.find("p").text()).toEqual("Hello, Unknown!");
+  expect(wrapper.find('p').text()).toEqual('Hello, Unknown!');
 });
