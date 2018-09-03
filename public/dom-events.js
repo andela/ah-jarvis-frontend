e7fa292c0854a1f40920508abfed5c22dcfccc35
DennisWanjiru <denniswanjiru71@gmail.com>
2018-09-03 22:20:11 +0300

    [ch #59961946] Remove unnecessary scss files and add materialize from npm

--- public/dom-events.js
+++
@@ -1,1 +0,0 @@
-// This is where materialize dom event go -> e.g dropdown
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'materialize-css/dist/css/materialize.min.css';
 
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
