13eadf37a0d7efb4d6a5953b06fc84a26f764b36
DennisWanjiru <denniswanjiru71@gmail.com>
2018-09-03 16:22:54 +0300

    [ch #59961946] Add test coverage scope

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Routes from './routes';
-import './styles/css/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
