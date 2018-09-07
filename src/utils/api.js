ee66752e2d8fd1cd57c01c4d6d7be4ed44003568
asamisellah <asami.rachael@gmail.com>
2018-09-07 11:36:55 +0300

    [ft #159961949] Resolve merge conflicts

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
-import config from "../config";
+import config from '../config';
 
 const handleResponse = response =>
-  response.text().then(text => {
+  response.text().then((text) => {
     const data = text && JSON.parse(text);
     if (!response.ok) {
       return Promise.reject(data);
@@ -11,14 +11,10 @@ const handleResponse = response =>
 
 export function authHeader(authenticated) {
   // return authorization header with jwt token
-<<<<<<< HEAD
-  const data = localStorage.getItem("user");
-=======
   if (!authenticated) {
     return {};
   }
   const data = localStorage.getItem('user');
->>>>>>> 76e951c0c05a9e9721e3a31a7193432ba0f65b2b
   if (!data) {
     return {};
   }
@@ -29,29 +25,16 @@ export function authHeader(authenticated) {
   return {};
 }
 
-<<<<<<< HEAD
-const api = ({ endpoint, method, data }) =>
+const api = ({
+  endpoint, method, data, authenticated,
+}) =>
   fetch(`${config.BASE_URL}${endpoint}`, {
     method,
     headers: {
-      "content-type": "application/json",
-      ...authHeader()
+      'content-type': 'application/json',
+      ...authHeader(authenticated),
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   }).then(handleResponse);
-=======
-
-const api = ({
-  endpoint, method, data, authenticated,
-}) => fetch(`${config.BASE_URL}${endpoint}`, {
-  method,
-  headers: {
-    'content-type': 'application/json',
-    ...authHeader(authenticated),
-  },
-  body: JSON.stringify(data),
-}).then(handleResponse);
-
->>>>>>> 76e951c0c05a9e9721e3a31a7193432ba0f65b2b
 
 export default api;
