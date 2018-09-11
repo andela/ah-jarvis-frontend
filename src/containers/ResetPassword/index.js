a3ed3ce945340046de34e9b8e3aa44f550a7f432
Tittoh <tittohb@gmail.com>
2018-09-11 12:27:44 +0300

    [ft

--- src/containers/ResetPassword/index.js
+++ src/containers/ResetPassword/index.js
@@ -2,21 +2,31 @@
 import { bindActionCreators } from 'redux';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import InputField from '../../components/InputField';
+import validateInput from '../../utils/validateInput';
+import Errors from '../../components/Errors';
 import resetPasswordAction from './actions';
 
 
 class ResetPassword extends Component {
+  static defaultProps = {
+    reset: {},
+    location: {},
+    match: {},
+  }
+
   state = {
     new_password: '',
     confirm_password: '',
+    validation: {},
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
     const {
-      resetPass, history, location, match,
+      resetPass, location, match,
     } = this.props;
 
     resetPass(
@@ -27,24 +37,38 @@ class ResetPassword extends Component {
           email: new URLSearchParams(location.search).get('email'),
         },
       },
-      history,
     );
   }
 
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value,
+      validation: { [e.target.name]: validateInput('password', e.target.value) },
+    });
   }
 
   validateForm() {
     return (
-      this.state.confirm_password.length > 0
-      && this.state.new_password.length > 0
+      this.state.confirm_password.length > 7
+      && this.state.new_password.length > 7
       && this.state.new_password === this.state.confirm_password
     );
   }
 
+  renderInput = (name, failure, errors, value, label, type) => (
+    <InputField
+      name={`${name}`}
+      label={`${label}`}
+      type={`${type}`}
+      value={value}
+      onChange={this.handleChange}
+      failure={failure}
+      errors={errors}
+    />
+  )
+
   render() {
-    const { errors, failure, isFetching } = this.props.resetPasswordAction;
+    const { errors, failure, isFetching } = this.props.reset;
     return (
       <div className="row">
         <div className="col m4 s12 offset-m4 auth">
@@ -53,22 +77,14 @@ class ResetPassword extends Component {
               <span className="card-title center-align text-primary  brand m-b--30 m-t--15">
                 Authors' Haven
               </span>
-              {failure && <span className="red-text">{errors.errors.new_password}</span>}
               <form onSubmit={this.handleSubmit}>
-                <InputField
-                  name="new_password"
-                  type="password"
-                  className="validate"
-                  value={this.state.new_password}
-                  label="Enter new password"
-                  onChange={this.handleChange}
-                />
-                <InputField
-                  name="confirm_password"
-                  type="password"
-                  value={this.state.confirm_password}
-                  label="Confirm new password"
-                  onChange={this.handleChange}
+                {this.renderInput('new_password', failure, errors, this.state.new_password, 'New Password', 'password')}
+                {<span className="red-text helper-text">{this.state.validation.new_password}</span>}
+                {this.renderInput('confirm_password', failure, errors, this.state.confirm_password, 'Confirm new password', 'password')}
+                <Errors
+                  name="error"
+                  errors={errors}
+                  failure={failure}
                 />
                 <div className="row">
                   <div className="input-field col s12">
@@ -91,8 +107,24 @@ class ResetPassword extends Component {
   }
 }
 
+ResetPassword.propTypes = {
+  resetPass: PropTypes.func.isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.isRequired,
+  }),
+  match: PropTypes.shape({
+    params: PropTypes.isRequired,
+  }),
+  reset: PropTypes.shape({
+    success: PropTypes.isRequired,
+    errors: PropTypes.isRequired,
+    failure: PropTypes.isRequired,
+    isFetching: PropTypes.isRequired,
+  }),
+};
+
 const mapStateToProps = state => ({
-  resetPasswordAction: state.resetPasswordAction,
+  reset: state.reset,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
-import forgotPasswordAction from './containers/ForgotPassword/reducer';
-import resetPasswordAction from './containers/ResetPassword/reducer';
+import forgotPass from './containers/ForgotPassword/reducer';
+import reset from './containers/ResetPassword/reducer';
 import signin from './containers/Signin/reducer';
 
 const message = () => ({ message: 'This is a reducer' });
 
 const rootReducer = combineReducers({
-  message, signin, forgotPasswordAction, resetPasswordAction,
+  message, signin, forgotPass, reset,
 });
 
 export default rootReducer;
