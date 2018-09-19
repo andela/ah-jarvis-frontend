be0852ed2320ff71abb70d3ef369c7270caba87c
desshub <desshub95@gmail.com>
2018-09-19 15:24:23 +0300

    [ft #160611847] Add not found articles component

--- src/containers/Articles/index.js
+++ src/containers/Articles/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
@@ -12,6 +13,7 @@ import Paginator from '../../components/Pagination';
 import config from '../../config';
 import Sidebar from '../Sidebar';
 import readTime from '../../utils/readtime';
+import ROUTES from '../../utils/routes';
 
 class Articles extends Component {
   constructor(props) {
@@ -60,6 +62,27 @@ class Articles extends Component {
     }, 500);
   }
 
+  renderNotFound = () => (
+    <div className="row">
+      <div className="col m8 s12 offset-m2">
+        <div className="card card--auth p-b--40">
+          <div className="card-content">
+            <span className="card-title center-align text-primary brand m-b--30 m-t--15">
+              OOP'S 404
+            </span>
+            <div className="center">
+              <p className="centre-align">No Articles found matching your keywords.</p>
+              <p className="m-t--15">Please search again.</p>
+            </div>
+          </div>
+          <Link className="btn waves-effect waves-light btn--block" to={ROUTES.articles}>
+            View All Artilces
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+
   renderArticles = () => {
     const { results } = this.props.articles.payload;
     const articles = results.map((data) => {
@@ -88,6 +111,10 @@ class Articles extends Component {
         />
       );
     });
+
+    if (articles.length === 0) {
+      return this.renderNotFound();
+    }
     return articles;
   };
 
