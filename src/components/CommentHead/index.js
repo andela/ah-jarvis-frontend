cbaec4a92a11f95f24d5b728c577cbab7e151e42
Tittoh <tittohb@gmail.com>
2018-09-20 19:02:04 +0300

    [ft #159961957] Fix missing user image on comment head

--- src/components/CommentHead/index.js
+++ src/components/CommentHead/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import capitalize from '../../utils/capitalize';
+import config from '../../config';
 
 const CommentHead = ({ user, date }) => (
   <div className="row comment-box__user">
-    <img src={user.image} alt="" className="responsive-img circle" />
+    <img
+      src={user.image ? user.image : config.DEFAULT_USER_AVATAR}
+      alt=""
+      className="responsive-img small--avatar circle-img"
+    />
     <div className="name">
       <p>{capitalize(user.username)}</p>
       <p className="p-t--10 grey-text">
@@ -24,6 +29,7 @@ export default CommentHead;
 CommentHead.propTypes = {
   user: PropTypes.shape({
     username: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
   }).isRequired,
   date: PropTypes.string.isRequired,
 };
